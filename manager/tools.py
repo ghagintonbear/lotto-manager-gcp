ecb372a8d7c6f62e967054fa8ede5210bfa175d7
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2020-12-15 22:09:14 +0000

    implemented folder management logic

---
+++ manager/tools.py
@@ -0,0 +1,60 @@
+import os
+from datetime import timedelta, datetime
+
+import pandas as pd
+
+
+def get_last_friday_date(date: datetime) -> (datetime, str):
+    """
+    given any date, function determines the date which the last friday occurred on.
+    """
+    weekday_num = date.isoweekday()
+    if weekday_num > 5:
+        diff = weekday_num - 5
+    elif weekday_num < 5:
+        diff = 7 - (5 - weekday_num)
+    else:
+        diff = 0
+    last_fri = date - timedelta(days=diff)
+    last_fri_str = f"Fri {last_fri:%d}" + f" {last_fri:%B}"[0:4] + f" {last_fri:%Y}"
+    return last_fri, last_fri_str
+
+
+def get_folder_name(selected_date: datetime) -> str:
+    """
+    organise generated results into date range folders within ../results_archive.
+    `date_range_start(end)` provided by user.
+    if selected_date > end_range then it goes in the folder that starts there.
+    """
+    def date_to_str(date: datetime) -> str: return f"{date:%Y-%m-%d}"
+
+    date_range_start = datetime(2020, 1, 10)
+    date_range_end = datetime(2030, 12, 31)
+    date_range_df = pd.DataFrame({
+        'start': pd.date_range(date_range_start, date_range_end, freq='28D'),
+        'end': pd.date_range(date_range_start + timedelta(days=28), date_range_end + timedelta(days=28), freq='28D')
+    })
+    for col in date_range_df.columns:
+        assert all(date_range_df[col].dt.day_name() == 'Friday'), f'date_range_df: {col} dates do not land on a friday'
+
+    selected_date = pd.to_datetime(selected_date)
+    folder_start_date, folder_end_date = date_range_df[date_range_df['end'] >= selected_date].iloc[0, :]
+    folder_name = date_to_str(folder_start_date) + '__' + date_to_str(folder_end_date)
+
+    return folder_name
+
+
+def make_results_folder(folder_name: str) -> str:
+    """
+    makes sure appropriate directories exist to store generated results.
+    """
+    results_folder = 'result_archive'
+
+    if results_folder not in os.listdir():
+        os.mkdir(f'./{results_folder}')
+
+    path_to_new_folder = f'./{results_folder}/{folder_name}'
+    if folder_name not in os.listdir(f'./{results_folder}/'):
+        os.mkdir(path_to_new_folder)
+
+    return path_to_new_folder
---
+++ tests/test_tools.py
@@ -0,0 +1,39 @@
+import datetime
+
+import pytest
+
+from manager.tools import get_last_friday_date, get_folder_name
+
+
+@pytest.mark.parametrize(
+    'today, last_fri, last_fri_str',
+    [
+        (datetime.datetime(2020, 12, 10), datetime.datetime(2020, 12, 4), 'Fri 04 Dec 2020'),
+        (datetime.datetime(2020, 12, 11), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020'),
+        (datetime.datetime(2020, 12, 12), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020'),
+        (datetime.datetime(2020, 12, 13), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020'),
+        (datetime.datetime(2020, 12, 14), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020'),
+        (datetime.datetime(2020, 12, 15), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020'),
+        (datetime.datetime(2020, 12, 16), datetime.datetime(2020, 12, 11), 'Fri 11 Dec 2020')
+    ]
+)
+def test_get_fri_date_str(today, last_fri, last_fri_str):
+    assert get_last_friday_date(today) == (last_fri, last_fri_str)
+
+
+@pytest.mark.parametrize(
+    'date, folder_name',
+    [
+        (datetime.date(2020, 2, 16), '2020-02-07__2020-03-06'),
+        (datetime.date(2020, 3, 23), '2020-03-06__2020-04-03'),
+        (datetime.date(2020, 6, 11), '2020-05-29__2020-06-26'),
+        (datetime.date(2020, 8, 21), '2020-07-24__2020-08-21'),
+        (datetime.date(2020, 9, 17), '2020-08-21__2020-09-18'),
+        (datetime.date(2020, 10, 4), '2020-09-18__2020-10-16'),
+        (datetime.date(2021, 11, 16), '2021-11-12__2021-12-10'),
+        (datetime.date(2021, 12, 5), '2021-11-12__2021-12-10'),
+        (datetime.date(2022, 4, 9), '2022-04-01__2022-04-29')
+    ]
+)
+def test_get_fri_date_str(date, folder_name):
+    assert get_folder_name(date) == folder_name
