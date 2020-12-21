24fb319081aaf2ab9f2ec48ba714d87a8b3c2a6a
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2020-12-21 20:04:34 +0000

    added func for new aggregation of results. #1 refactored old logic to be more testable.

--- manager/cumulate_results.py
+++ manager/cumulate_results.py
@@ -3,24 +3,58 @@ import glob
 import pandas as pd
 
 
-def compute_cumulative_result(path_to_results='.\\result_archive\\*\\*.xlsx'):
-    cumulative = {}
+def compute_cumulated_result(path_to_results='.\\result_archive\\*\\*.xlsx'):
+    general_overview = {}
+    player_prize_breakdown = {}
     for xlsx_file_path in glob.glob(path_to_results):
         _, play_interval, filename = xlsx_file_path.rsplit('\\', maxsplit=2)
         play_date = filename.rsplit('.', maxsplit=1)[0]
         result = pd.read_excel(xlsx_file_path, sheet_name='Result', engine='openpyxl')
         result['Prize'] = result['Prize'].str.replace('£', '').astype(float)
-        cumulative['Play Date'] = cumulative.get('Play Date', []) + [play_date]
-        cumulative['Interval'] = cumulative.get('Interval', []) + [play_interval]
-        cumulative['Winnings'] = cumulative.get('Winnings', []) + [result['Prize'].sum()]
-        cumulative['Num of Players'] = cumulative.get('Num of Players', []) + [result.shape[0]]
-        cumulative['Winning per Person'] = cumulative.get('Winning per Person', []) + [
-            cumulative['Winnings'][-1] / cumulative['Num of Players'][-1]
-        ]
-        cumulative['Cumulative Winnings per Person'] = cumulative.get('Cumulative Winnings per Person', []) + [
-            sum(cumulative['Winning per Person'])
-        ]
-        cumulative['Winning Description'] = cumulative.get('Winning Description', []) + [
-            result[result['Prize'] > 0]['Match Type'].str.cat(sep='; ')
-        ]
-    return cumulative
+
+        general_overview = calculate_general_overview_row(
+            data=result, store=general_overview, play_interval=play_interval, play_date=play_date
+        )
+
+        player_prize_breakdown = calculate_player_prize_breakdown(
+            data=result, store=player_prize_breakdown, play_interval=play_interval, play_date=play_date
+        )
+
+    player_prize_breakdown.pop('players')
+
+    return pd.DataFrame(general_overview), pd.DataFrame(player_prize_breakdown)
+
+
+def calculate_general_overview_row(data: pd.DataFrame, store: dict, play_interval: str, play_date: str) -> dict:
+    store['Interval'] = store.get('Interval', []) + [play_interval]
+    store['Play Date'] = store.get('Play Date', []) + [play_date]
+    store['Winnings'] = store.get('Winnings', []) + [data['Prize'].sum()]
+    store['Num of Players'] = store.get('Num of Players', []) + [data.shape[0]]
+    store['Winning per Person'] = store.get('Winning per Person', []) + [
+        store['Winnings'][-1] / store['Num of Players'][-1]
+    ]
+    store['Winning Description'] = store.get('Winning Description', []) + [
+        data[data['Prize'] > 0]['Match Type'].str.cat(sep='; ')
+    ]
+
+    return store
+
+
+def calculate_player_prize_breakdown(data: pd.DataFrame, store: dict, play_interval: str, play_date: str) -> dict:
+    store['players'] = set(store.get('players', [])).union(data['Name'].values)
+    store['Interval'] = store.get('Interval', []) + [play_interval]
+    store['Play Date'] = store.get('Play Date', []) + [play_date]
+    expected_length = len(store['Play Date']) - 1
+    for player in store['players']:
+        current_length = len(store.get(player, []))
+        if current_length != expected_length:
+            store[player] = store.get(player, []) + [None] * (expected_length - current_length)
+
+        if player in data['Name'].values:
+            value = data['Prize'].sum()/data.shape[0]
+        else:
+            value = None
+
+        store[player] = store.get(player, []) + [value]
+
+    return store
