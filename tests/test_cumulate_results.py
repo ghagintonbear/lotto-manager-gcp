6699aa2e0ecc5d656e65b636e95fb40acf3363ea
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2020-12-23 21:08:16 +0000

    test_compute_cumulated_result #2

--- tests/test_cumulate_results.py
+++ tests/test_cumulate_results.py
@@ -1,7 +1,10 @@
+from unittest.mock import patch, MagicMock
+
 from pytest import fixture
+from pandas.testing import assert_frame_equal
 
 from manager.cumulate_results import (
-    calculate_general_overview_row, calculate_player_prize_breakdown
+    calculate_general_overview_row, calculate_player_prize_breakdown, compute_cumulated_result
 )
 
 
@@ -112,3 +115,49 @@ def test_calculate_player_prize_breakdown_non_empty_store(result_data, play_inte
     outcome = calculate_player_prize_breakdown(result_data, pre_store, play_interval, play_date)
 
     assert outcome == expected
+
+
+def mock_glob(path):
+    return ['.\\test\\int_a\\date_1.xlsx', '.\\test\\int_b\\date_2.xlsx', '.\\test\\int_c\\date_3.xlsx']
+
+
+@patch('glob.glob', mock_glob)
+def test_compute_cumulated_result(result_data):
+    from pandas import DataFrame
+
+    result_data_2 = DataFrame({
+        'Name': ['Nivea', 'Cat', 'Shampoo'],
+        'Match Type': ['Match 3', 'Match 2', 'Match 9'],
+        'Prize': ["£1.00", "£2.00", "£0.00"]
+    })
+    result_data_3 = DataFrame({
+        'Name': ['Nivea', 'Baby', 'Shampoo'],
+        'Match Type': ['', '', ''],
+        'Prize': ["£0.00", "£0.00", "£0.00"]
+    })
+
+    expected_overview = DataFrame({
+        'Interval': ['int_a', 'int_b', 'int_c'],
+        'Play Date': ['date_1', 'date_2', 'date_3'],
+        'Winnings': [100006.99, 3.0, 0],
+        'Num of Players': [3, 3, 3],
+        'Winning per Person': [33335.66333333334, 1.0, 0],
+        'Winning Description': ['Match 3 + 2 Stars; Jackpot', 'Match 3; Match 2', ''],
+    })
+
+    expected_breakdown = DataFrame({
+        'Interval': ['int_a', 'int_b', 'int_c'],
+        'Play Date': ['date_1', 'date_2', 'date_3'],
+        'Shampoo': [33335.66333333334, 1.0, 0],
+        'Baby': [33335.66333333334, None, 0],
+        'Johnson': [33335.66333333334, None, None],
+        'Cat': [None, 1.0, None],
+        'Nivea': [None, 1.0, 0]
+    })
+
+    with patch('pandas.read_excel', MagicMock(side_effect=[result_data, result_data_2, result_data_3])):
+        overview, breakdown = compute_cumulated_result()
+        assert_frame_equal(overview[overview.columns.sort_values()],
+                           expected_overview[expected_overview.columns.sort_values()])
+        assert_frame_equal(breakdown[breakdown.columns.sort_values()],
+                           expected_breakdown[expected_breakdown.columns.sort_values()])
