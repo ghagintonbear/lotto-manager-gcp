483d23384783c1a1bbe7de3e04d86d2f015387a0
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2020-12-28 13:18:16 +0000

    base_url sits in scrape_results.py. filepaths to key files stays at high level.

--- manager/__init__.py
+++ manager/__init__.py
@@ -13,9 +13,6 @@ from manager.tools import (
 from manager.gui import run_gui, update_label_log
 
 
-BASE_URL = 'https://www.national-lottery.co.uk'
-
-
 def run_manager(run_date: date, label):
     selected = get_selected_numbers()
 
@@ -23,7 +20,7 @@ def run_manager(run_date: date, label):
     folder_name = get_folder_name(draw_date)
     folder_path = make_results_folder(folder_name)
 
-    draw_result, prize_breakdown = get_draw_information(BASE_URL, draw_date)
+    draw_result, prize_breakdown = get_draw_information(draw_date)
     winning_numbers = collect_winning_numbers(draw_result)
 
     results = check_matches_on_selected(selected, winning_numbers, prize_breakdown)
@@ -36,10 +33,10 @@ def run_manager(run_date: date, label):
     return results
 
 
-def get_draw_information(base_url: str, draw_date: date) -> (dict, dict):
-    historical_data = scrape_historical_results(base_url)
+def get_draw_information(draw_date: date) -> (dict, dict):
+    historical_data = scrape_historical_results()
     draw_result = extract_draw_result(draw_date, historical_data)
-    prize_breakdown = scrape_prize_breakdown(base_url, draw_result['DrawNumber'])
+    prize_breakdown = scrape_prize_breakdown(draw_result['DrawNumber'])
     return draw_result, prize_breakdown
 
 
@@ -49,19 +46,24 @@ def run_manager_between(start: date, end: date, label):
 
 
 def produce_cumulative_report():
+    file_path = './Master Results.xlsx'
+
     general_overview, player_breakdown = compute_cumulated_result()
 
     player_summary = player_breakdown.groupby('Interval', as_index=False).sum()
     player_summary = add_sum_row('Interval', player_summary)
 
-    write_cumulative_result({
-        'Player Summary': (player_summary, player_summary.dtypes[player_summary.dtypes != 'object'].index),
-        'Player Breakdown': (player_breakdown, player_breakdown.dtypes[player_breakdown.dtypes != 'object'].index),
-        'General Overview': (general_overview, ['Winnings', 'Winning per Person'])
-    })
+    write_cumulative_result(
+        frames={
+            'Player Summary': (player_summary, player_summary.dtypes[player_summary.dtypes != 'object'].index),
+            'Player Breakdown': (player_breakdown, player_breakdown.dtypes[player_breakdown.dtypes != 'object'].index),
+            'General Overview': (general_overview, ['Winnings', 'Winning per Person'])
+        },
+        save_path=file_path
+    )
 
     if os.name == 'nt':
-        os.startfile(os.path.abspath('./Master Results.xlsx'))
+        os.startfile(os.path.abspath(file_path))
 
 
 def run_manager_with_gui():
--- manager/scrape_results.py
+++ manager/scrape_results.py
@@ -6,17 +6,19 @@ import requests
 from bs4 import BeautifulSoup as bSoup
 import pandas as pd
 
+BASE_URL = 'https://www.national-lottery.co.uk'
 
-def scrape_historical_results(base_url: str) -> pd.DataFrame:
+
+def scrape_historical_results(draw_history_path: str = '/results/euromillions/draw-history') -> pd.DataFrame:
     """ on draw history page, find href link to csv and use pandas to read it. """
-    draw_history_url_path = base_url + '/results/euromillions/draw-history'
+    draw_history_url_path = BASE_URL + draw_history_path
 
     draw_history_page = requests.get(draw_history_url_path)
     draw_history_soup = bSoup(draw_history_page.content, 'html.parser')
     href_id = 'download_history_action'
 
     try:
-        link_to_csv = base_url + draw_history_soup.find(id=href_id).get('href')
+        link_to_csv = BASE_URL + draw_history_soup.find(id=href_id).get('href')
     except AttributeError as E:
         logging.error(f'Failed to find html tag with id="{href_id}" in soup.')
         raise E
@@ -42,11 +44,11 @@ def extract_draw_result(draw_date: date, hist_results: pd.DataFrame) -> dict:
     return draw_result
 
 
-def scrape_prize_breakdown(base_url: str, draw_number: int) -> dict:
+def scrape_prize_breakdown(draw_number: int) -> dict:
     """ from the selected prize breakdown page, find the prize breakdown table and extract the
         No. of matches and the Prize per UK winner information into a dict as respective key: value pair."""
     breakdown_url_ext = f'/results/euromillions/draw-history/prize-breakdown/{draw_number}'
-    breakdown_page = requests.get(base_url + breakdown_url_ext)
+    breakdown_page = requests.get(BASE_URL + breakdown_url_ext)
     breakdown_soup = bSoup(breakdown_page.content, 'html.parser')
     prize_breakdown = {}
     for table in breakdown_soup.find_all('table'):
