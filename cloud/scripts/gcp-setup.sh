482b50b8812cbb310a41e2056d44970c59825ecb
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-03 17:05:34 +0000

    project needs account billing #3

--- cloud/scripts/gcp-setup.sh
+++ cloud/scripts/gcp-setup.sh
@@ -1,7 +1,25 @@
+#!/bin/bash
+set -e # if error exit
+
 # script variables
-PROJECT_ID="euro-lotto-manager-tes" # Project IDs must be between 6 and 30 characters.
+PROJECT_ID="euro-lotto-manager" # Project IDs must be between 6 and 30 characters.
 REGION="europe-west2"  # europe-west2 is London
 BUCKET_NAME="results-${PROJECT_ID}"
+
+# Check BILLING_ACCOUNT_ID is set
+if [ -n "${BILLING_ACCOUNT_ID}" ]; then
+    echo
+    echo "Using BILLING_ACCOUNT_ID environment variable."
+    echo
+else
+    echo
+    echo "*** ERROR ***"
+    echo "Please set BILLING_ACCOUNT_ID environment variable."
+    echo "You can find it here: https://console.cloud.google.com/billing"
+    echo "Exiting..."
+    echo
+    exit
+fi
 
 
 echo "*** STEP 1: Requesting for Authentication ***"
@@ -17,12 +35,18 @@ gcloud projects create ${PROJECT_ID}
 PROJECT_NUMBER=`gcloud projects list --filter=${PROJECT_ID} --format="value(PROJECT_NUMBER)"`
 
 echo
-echo "*** STEP 3: Selecting Project ***"
+echo "*** STEP 3: Link Project to billing account ***"
+echo
+gcloud alpha billing projects link ${PROJECT_ID} \
+    --billing-account ${BILLING_ACCOUNT_ID}
+
+echo
+echo "*** STEP 4: Selecting Project ***"
 echo
 gcloud config set project ${PROJECT_ID}
 
 
 echo
-echo "*** INFO 4 Creating buckets ***"
+echo "*** STEP 5 Creating buckets ***"
 echo
 gsutil mb -c standard -l ${REGION} gs://${BUCKET_NAME}  # Where results will be stored
