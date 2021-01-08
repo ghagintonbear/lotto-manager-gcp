680bb103fa67b9b000ea3d0031473df5070708ac
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-08 21:08:04 +0000

    adapted to accept function created pubsub message #15

--- functions/run_manager/src/main.py
+++ functions/run_manager/src/main.py
@@ -2,8 +2,8 @@ import base64
 from datetime import datetime, date
 
 from manager.bigquery import read_selected_numbers, establish_results_in_bigquery
-from manager.scrape_results import scrape_historical_results, scrape_prize_breakdown, extract_draw_result
 from manager.check_matches import collect_winning_numbers, check_matches_on_selected
+from manager.scrape_results import scrape_historical_results, scrape_prize_breakdown, extract_draw_result
 from manager.tools import get_last_friday_date
 
 
@@ -21,9 +21,17 @@ def run_manager(event, _context):
         pubsub_message = base64.b64decode(event['data']).decode('utf-8')
         print(f'PubSub event: "{pubsub_message}"')
 
+    if 'attributes' in event:
+        print(f'PubSub attributes: attributes={event["attributes"]}')
+        if 'run_date' in event["attributes"]:
+            run_date_str = event["attributes"]['run_date']
+            run_date = datetime.strptime(run_date_str, '%Y-%m-%d')
+        else:
+            run_date = datetime.now().date()
+
     selected = read_selected_numbers()
 
-    draw_date, draw_date_str = get_last_friday_date(datetime.now().date())
+    draw_date, draw_date_str = get_last_friday_date(run_date)
 
     draw_result, prize_breakdown = get_draw_information(draw_date)
     winning_numbers = collect_winning_numbers(draw_result)
