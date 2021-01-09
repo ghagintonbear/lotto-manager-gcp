c0c1843b467fd7f04a06f394dacd4a5b847cf473
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-09 18:22:25 +0000

    initial outline of logic to cumulate_results #19

--- manager/bigquery/__init__.py
+++ manager/bigquery/__init__.py
@@ -1,84 +1,11 @@
 import os
+import re
 
 import pandas as pd
 import google.cloud.bigquery as bq
-from google.cloud.exceptions import NotFound
 
-from manager.tools import assert_values_in_range
-
-
-def read_selected_numbers():
-    """ Read manager.selected_numbers ('./selected_numbers.csv') from BigQuery and validates them.
-        Ensures numbers selected are valid and there are no duplicates in Name col.
-    """
-    select_all_query = f"""
-    SELECT *
-    FROM `{os.getenv('PROJECT_ID')}.manager.selected_numbers`"""
-    selected_numbers = pd.read_gbq(select_all_query)
-
-    if not selected_numbers['Name'].is_unique:
-        duplicates = selected_numbers['Name'].value_counts()
-        raise ValueError(f'"Name" in Selected numbers needs to be unique. Correct:\n{duplicates[duplicates > 1]}')
-
-    number_cols = [col for col in selected_numbers.columns if col.startswith('Number_')]
-    assert_values_in_range(selected_numbers, start=1, end=50, cols=number_cols)
-
-    star_cols = [col for col in selected_numbers.columns if col.startswith('Lucky_Star_')]
-    assert_values_in_range(selected_numbers, start=1, end=12, cols=star_cols)
-
-    return selected_numbers
-
-
-def write_dictionary_to_bigquery(client: bq.Client, data: dict, col_names: list, table_name: str, dataset_name: str):
-
-    data = dict(zip(col_names, [data.keys(), data.values()]))
-    data = pd.DataFrame.from_dict(data, orient='columns')
-
-    write_dataframe_to_bigquery(client, data=data.astype(str), table_name=table_name, dataset_name=dataset_name)
-    return
-
-
-def write_dataframe_to_bigquery(client: bq.Client, data: pd.DataFrame, table_name: str, dataset_name: str):
-    table_id = '.'.join([os.getenv('PROJECT_ID'), dataset_name, table_name])
-
-    job_config = bq.LoadJobConfig(
-        # Specify a (partial) schema. All columns are always written to the
-        # table. The schema is used to assist in data type definitions.
-        schema=[
-            # pandas dtype "object" is ambiguous and cannot be auto-detected.
-            bq.SchemaField(col, bq.enums.SqlTypeNames.STRING) for col in data.dtypes[data.dtypes == 'object'].index
-        ],
-        # WRITE_TRUNCATE replaces the table with the loaded data.
-        write_disposition="WRITE_TRUNCATE",
-    )
-
-    job = client.load_table_from_dataframe(
-        data,
-        table_id,
-        job_config=job_config
-    )  # Make an API request.
-    job.result()  # Wait for the job to complete.
-
-    table = client.get_table(table_id)  # Make an API request.
-    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
-    return
-
-
-def create_bigquery_dataset(client: bq.Client, dataset_name: str):
-    dataset_id = os.getenv('PROJECT_ID') + '.' + dataset_name
-
-    try:
-        client.get_dataset(dataset_id)  # Make an API request.
-        print(f"Dataset {dataset_id} already exists")
-
-    except NotFound:
-        print(f"Dataset {dataset_id} is not found. Attempting to create it.")
-        dataset = bq.Dataset(dataset_id)
-        dataset.location = os.getenv('REGION')
-        dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
-        print(f"Created dataset {client.project}.{dataset.dataset_id}")
-
-    return
+from manager.bigquery.read import read_selected_numbers
+from manager.bigquery.write import create_bigquery_dataset, write_dataframe_to_bigquery, write_dictionary_to_bigquery
 
 
 def establish_results_in_bigquery(dataset_name: str, results: pd.DataFrame, draw_result: dict, prize_breakdown: dict):
@@ -95,4 +22,32 @@ def establish_results_in_bigquery(dataset_name: str, results: pd.DataFrame, draw
                                  table_name='prize_breakdown', dataset_name=dataset_name)
 
     print(f'Successfully written all results to BigQuery {os.getenv("PROJECT_ID")}.{dataset_name}')
+    return
+
+
+def cumulate_results():
+    bq_client = bq.Client()
+
+    datasets_with_results = []
+    for dataset_list_item in bq_client.list_datasets():  # Make an API request.
+        dataset_id = dataset_list_item.dataset_id
+        if re.fullmatch(r'\d{4}_\d{2}_\d{2}_\w*_Fri', dataset_id):
+            if 'results' in list(bq_client.list_tables(dataset=dataset_id)):
+                datasets_with_results.append(dataset_id)
+
+    for dataset_id in datasets_with_results:
+        # compound queries which will:
+        #   add a column to each table (not perm) with run_date
+        #   append all the results
+        #   filter out prize != $0.00
+        #   convert prize to NUMERIC type
+        #   groupby run_date
+        #       sum prize
+        #       collect match types
+        #       count number of players
+        #       calculate winnings per person
+        # Do similar of player_breakdown table
+        pass
+
+    # write out result to manager
     return
