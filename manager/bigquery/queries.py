4705d4dadaaef37f29f28550b4e0b8cd0ddc8078
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-10 16:49:13 +0000

    fixed bugs in general_summary_query #19

--- manager/bigquery/queries.py
+++ manager/bigquery/queries.py
@@ -24,25 +24,53 @@ def create_general_summary_query(dataset_ids: list) -> str:
 
 
 def _query_for_general_summary(dataset_id: str) -> str:
+    """
+    * results_with_new_columns:
+        select columns of interest and create Play_Date col
+        convert Prize (type string, [£,.0-9]) to int to avoid rounding errors
+        add Num_of_Players which is the length of the data by design
+
+    * filtered_match_types - helper query
+
+    * winning_match_types -> 1x1 table:
+        EXISTS checks if query result is empty or not
+        It only returns True or False
+
+    * collapsed_to_one_row -> 4x1 table:
+        aggregates results
+
+    * summary:
+        converts ints (pence) into float (pounds) after aggregation
+    """
     return \
-        f"""results_with_new_columns AS (
-                SELECT Match_Type, '{dataset_id}' AS Play_Date, 
-                    CAST(REGEXP_REPLACE(prize, r"[\D]+", "") AS INT64) AS Winnings,
+        f"""results_with_new_columns_{dataset_id} AS (
+                SELECT Match_Type, '{dataset_id}' AS Play_Date,
+                    CAST(REGEXP_REPLACE(Prize, r"[\D]+", "") AS INT64) AS Winnings,
                     (select COUNT(*) from `{dataset_id}.results`) AS Num_of_Players
                 FROM `{dataset_id}.results`
             ),
-            only_winnings_{dataset_id} AS (
-                SELECT *
-                FROM results_with_new_columns
+            filtered_match_types_{dataset_id} AS (
+                SELECT Match_Type 
+                FROM results_with_new_columns_{dataset_id}
                 WHERE Winnings > 0
             ),
+            winning_match_types_{dataset_id} AS (
+                SELECT
+                    CASE 
+                    WHEN EXISTS(SELECT Match_Type FROM filtered_match_types_{dataset_id})
+                      THEN STRING_AGG(Match_Type, '; ')
+                      ELSE NULL
+                    END AS Winning_Match_Type
+                FROM filtered_match_types_{dataset_id}
+            ),
             collapsed_to_one_row_{dataset_id} AS(
-                SELECT DISTINCT Play_Date, Winnings, Num_of_Players, 
-                        ((select STRING_AGG(Match_Type, '; ') from `only_winnings_{dataset_id}`)) AS Winning_Description
-                FROM only_winnings_{dataset_id}
+                SELECT DISTINCT Play_Date, Num_of_Players, 
+                                (SELECT Winning_Match_Type FROM winning_match_types_{dataset_id}) AS Winning_Match_Type,
+                                (SELECT SUM(Winnings) AS Total_Winnings FROM results_with_new_columns_{dataset_id}) AS Total_Winnings
+                FROM results_with_new_columns_{dataset_id}
             ),
             summary_{dataset_id} AS (
-                SELECT Play_Date, Num_of_Players, (Winnings / 100) AS Winnings, 
-                (Winnings / (100 * Num_of_Players)) AS Winnings_per_Player, Winning_Description
+                SELECT Play_Date, Num_of_Players, (Total_Winnings / 100) AS Total_Winnings, 
+                (Total_Winnings / (100 * Num_of_Players)) AS Winnings_per_Player, Winning_Match_Type
                 FROM collapsed_to_one_row_{dataset_id}
             )"""
