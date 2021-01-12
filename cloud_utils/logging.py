bea9ba66a0352708a44056fa57690dece16e8629
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-12 23:47:41 +0000

    1st attempt to use cloud logging api #23

---
+++ cloud_utils/logging.py
@@ -0,0 +1,16 @@
+import os
+from typing import Literal
+
+from google.cloud.logging import Resource, Client
+
+resource = Resource(type="cloud_function")
+
+logging_client = Client()
+cloud_logger = logging_client.logger(os.getenv('PROJECT_ID'))
+
+
+def cloud_log(message: str, level: str = Literal['info', 'warning', 'error']) -> None:
+
+    cloud_logger.log_struct({'level': level, 'message': message}, resource=resource)
+
+    return
