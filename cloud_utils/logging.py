8247c4efb5f2b1cba31c32d8603be6d5ec66f2f1
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-13 07:32:53 +0000

    adding log to label #23

--- cloud_utils/logging.py
+++ cloud_utils/logging.py
@@ -3,14 +3,13 @@ from typing import Literal
 
 from google.cloud.logging import Resource, Client
 
-resource = Resource(type="cloud_function")
+resource = Resource(type="cloud_function", labels={"user_defined": "us-central1-a"})
 
 logging_client = Client()
 cloud_logger = logging_client.logger(os.getenv('PROJECT_ID'))
 
 
 def cloud_log(message: str, level: str = Literal['info', 'warning', 'error']) -> None:
-
     cloud_logger.log_struct({'level': level, 'message': message}, resource=resource)
 
     return
