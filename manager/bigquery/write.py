a4ce7fb875556c1c001116cd06eda6c844dcc1c7
ghagintonbear <47527100+ghagintonbear@users.noreply.github.com>
2021-01-17 15:37:32 +0000

    minor edit, functions now return message for test assertion

--- manager/bigquery/write.py
+++ manager/bigquery/write.py
@@ -16,9 +16,9 @@ def write_dictionary_to_bigquery(
     return
 
 
-def write_dataframe_to_bigquery(client: bq.Client, data: pd.DataFrame, table_name: str, dataset_name: str) -> None:
+def write_dataframe_to_bigquery(client: bq.Client, data: pd.DataFrame, table_name: str, dataset_name: str) -> str:
     """ Writes Pandas Dataframe to given BigQuery dataset_name. Always overwrites existing data if any. """
-    table_id = '.'.join([os.getenv('PROJECT_ID'), dataset_name, table_name])
+    table_id = '.'.join([client.project, dataset_name, table_name])
 
     job_config = bq.LoadJobConfig(
         # Specify a (partial) schema. All columns are always written to the
@@ -39,23 +39,25 @@ def write_dataframe_to_bigquery(client: bq.Client, data: pd.DataFrame, table_nam
     job.result()  # Wait for the job to complete.
 
     table = client.get_table(table_id)  # Make an API request.
-    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
-    return
+    result = f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}"
+    print(result)
+    return result
 
 
-def create_bigquery_dataset(client: bq.Client, dataset_name: str) -> None:
+def create_bigquery_dataset(client: bq.Client, dataset_name: str) -> str:
     """ Creates BigQuery dataset, if one doesn't exist already. """
-    dataset_id = os.getenv('PROJECT_ID') + '.' + dataset_name
+    dataset_id = client.project + '.' + dataset_name
 
     try:
         client.get_dataset(dataset_id)  # Make an API request.
-        print(f"Dataset {dataset_id} already exists")
+        result = f"Dataset {dataset_id} already exists"
 
     except NotFound:
         print(f"Dataset {dataset_id} is not found. Attempting to create it.")
         dataset = bq.Dataset(dataset_id)
         dataset.location = os.getenv('REGION')
         dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
-        print(f"Created dataset {client.project}.{dataset.dataset_id}")
+        result = f"Created dataset {client.project}.{dataset.dataset_id}"
 
-    return
+    print(result)
+    return result
